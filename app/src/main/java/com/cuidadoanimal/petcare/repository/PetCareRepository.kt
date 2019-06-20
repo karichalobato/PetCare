8d76e50a3ea13d9d5e1b3d57d64482ef1d358148
Tania Orellana <00123717@uca.edu.sv>
2019-06-20 11:54:24 -0600

    Corregir métodos de repositorio. Utilizar anotación WorkerThread.

--- app/src/main/java/com/cuidadoanimal/petcare/repository/PetCareRepository.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/repository/PetCareRepository.kt
@@ -5,16 +5,25 @@ import androidx.lifecycle.LiveData
 import com.cuidadoanimal.petcare.database.entities.*
 import com.cuidadoanimal.petcare.database.entitiesDAO.*
 
-class PetCareRepository (private val UserDAO:UserDAO,private val PetDAO:PetDAO,private val ArticleDAO:ArticleDAO, private val TagDAO:TagDAO,private val VaccineDAO:VaccineDAO,private val ArticleJOINTagDAO:ArticleJOINTagDAO, private val UserJOINArticleDAO:UserJOINArticleDAO,private val VaccineJOINPetDAO:VaccineJOINPetDAO) {
+class PetCareRepository(
+    private val UserDAO: UserDAO,
+    private val PetDAO: PetDAO,
+    private val ArticleDAO: ArticleDAO,
+    private val TagDAO: TagDAO,
+    private val VaccineDAO: VaccineDAO,
+    private val ArticleJOINTagDAO: ArticleJOINTagDAO,
+    private val UserJOINArticleDAO: UserJOINArticleDAO,
+    private val VaccineJOINPetDAO: VaccineJOINPetDAO
+) {
     //GetAll
-    val allUsers:LiveData<List<User>> = UserDAO.getAllUsers()
-    val allPets:LiveData<List<Pet>> = PetDAO.getAllPets()
-    val allTags:LiveData<List<Tag>> = TagDAO.getAllTags()
-    val allArticle:LiveData<List<Article>> = ArticleDAO.getAllArticles()
-    val allVaccines:LiveData<List<Vaccine>> = VaccineDAO.getAllVaccines()
-    val allVaccineJOINPets:LiveData<List<VaccineJOINPet>> = VaccineJOINPetDAO.getAllVaccineJOINPets()
-    val allUserJOINArticles:LiveData<List<UserJOINArticle>> = UserJOINArticleDAO. getAllUserJOINArticles()
-    val allArticleJOINTags:LiveData<List<ArticleJOINTag>> = ArticleJOINTagDAO.getAllArticleJOINTags()
+    val allUsers: LiveData<List<User>> = UserDAO.getAllUsers()
+    val allPets: LiveData<List<Pet>> = PetDAO.getAllPets()
+    val allTags: LiveData<List<Tag>> = TagDAO.getAllTags()
+    val allArticle: LiveData<List<Article>> = ArticleDAO.getAllArticles()
+    val allVaccines: LiveData<List<Vaccine>> = VaccineDAO.getAllVaccines()
+    val allVaccineJOINPets: LiveData<List<VaccineJOINPet>> = VaccineJOINPetDAO.getAllVaccineJOINPets()
+    val allUserJOINArticles: LiveData<List<UserJOINArticle>> = UserJOINArticleDAO.getAllUserJOINArticles()
+    val allArticleJOINTags: LiveData<List<ArticleJOINTag>> = ArticleJOINTagDAO.getAllArticleJOINTags()
 
     //User
     @WorkerThread
@@ -22,19 +31,21 @@ class PetCareRepository (private val UserDAO:UserDAO,private val PetDAO:PetDAO,p
         UserDAO.insertUser(user)
     }
 
-    fun setProjectImage(username: String,profile_picture:String){
-        UserDAO.setUserImage(username,profile_picture)
+    fun setProjectImage(username: String, profile_picture: String) {
+        UserDAO.setUserImage(username, profile_picture)
     }
 
-    fun getUserByName(name: String): LiveData<List<User>>{
+    fun getUserByName(name: String): LiveData<List<User>> {
         return UserDAO.getUserByName(name)
     }
 
-    fun deleteUsers(){
+    @WorkerThread
+    suspend fun deleteUsers() {
         UserDAO.deleteUsers()
     }
 
-    fun deleteUserByID(idUser: Int): LiveData<List<User>>{
+    @WorkerThread
+    suspend fun deleteUserByID(idUser: Int) {
         return UserDAO.deleteUserByID(idUser)
     }
 
@@ -44,73 +55,81 @@ class PetCareRepository (private val UserDAO:UserDAO,private val PetDAO:PetDAO,p
         PetDAO.insertPet(pet)
     }
 
-    fun geyPetByName(name:String): LiveData<List<Pet>>{
+    fun geyPetByName(name: String): LiveData<List<Pet>> {
         return PetDAO.geyPetByName(name)
     }
 
-    fun setPetImage(name: String,pet_photo:String){
-        PetDAO.setPetImage(name,pet_photo)
+    fun setPetImage(name: String, pet_photo: String) {
+        PetDAO.setPetImage(name, pet_photo)
     }
 
-    fun deletePetByID(idPet: Int): LiveData<List<Pet>>{
+    @WorkerThread
+    suspend fun deletePetByID(idPet: Int) {
         return PetDAO.deletePetByID(idPet)
     }
 
-    fun deletePets(){
+    @WorkerThread
+    suspend fun deletePets() {
         PetDAO.deletePets()
     }
 
     //Article
     @WorkerThread
-    suspend fun insertArticle(article:Article){
+    suspend fun insertArticle(article: Article) {
         ArticleDAO.insertArticle(article)
     }
 
-    fun getArticleByTitle(title:String): LiveData<List<Article>>{
+    fun getArticleByTitle(title: String): LiveData<List<Article>> {
         return ArticleDAO.getArticleByTitle(title)
     }
 
-    fun deleteArticleByID(idArticle:Int): LiveData<List<Article>>{
+    @WorkerThread
+    suspend fun deleteArticleByID(idArticle: Int) {
         return ArticleDAO.deleteArticleByID(idArticle)
     }
 
-    fun deleteArticles(){
+    @WorkerThread
+    suspend fun deleteArticles() {
         ArticleDAO.deleteArticles()
     }
 
     //TagDAO
     @WorkerThread
-    suspend fun insertTag(tag: Tag){
+    suspend fun insertTag(tag: Tag) {
         TagDAO.insertTag(tag)
     }
 
-    fun getTagByName(name:String): LiveData<List<Tag>>{
+    fun getTagByName(name: String): LiveData<List<Tag>> {
         return TagDAO.getTagByName(name)
     }
 
-    fun deleteTagByID(idTag:Int): LiveData<List<Tag>>{
+    @WorkerThread
+    suspend fun deleteTagByID(idTag: Int) {
         return TagDAO.deleteTagByID(idTag)
     }
 
-    fun deleteTags(){
+    @WorkerThread
+    suspend fun deleteTags() {
         TagDAO.deleteTags()
     }
 
     //VaccineDAO
     @WorkerThread
-    suspend fun insertVaccine(vaccine: Vaccine){
+    suspend fun insertVaccine(vaccine: Vaccine) {
         VaccineDAO.insertVaccine(vaccine)
     }
 
-    fun getVaccineByName(name: String): LiveData<List<Vaccine>>{
+    fun getVaccineByName(name: String): LiveData<List<Vaccine>> {
         return VaccineDAO.getVaccineByName(name)
     }
 
-    fun deleteVaccineByID(idVaccine: Int): LiveData<List<Vaccine>>{
+    @WorkerThread
+    suspend fun deleteVaccineByID(idVaccine: Int) {
         return VaccineDAO.deleteVaccineByID(idVaccine)
     }
 
-    fun deleteVaccines(){
+    @WorkerThread
+    suspend fun deleteVaccines() {
         VaccineDAO.deleteVaccines()
     }
 
@@ -121,38 +140,39 @@ class PetCareRepository (private val UserDAO:UserDAO,private val PetDAO:PetDAO,p
     }
 
     fun getVaccineOfPets(vacunaID: Int): LiveData<List<Vaccine>> {
-    return VaccineJOINPetDAO.getVaccineOfPets(vacunaID)
+        return VaccineJOINPetDAO.getVaccineOfPets(vacunaID)
 
     }
 
-    fun getPetsOfVaccines(mascotaID:Int):LiveData<List<Pet>>{
+    fun getPetsOfVaccines(mascotaID: Int): LiveData<List<Pet>> {
         return VaccineJOINPetDAO.getPetsOfVaccines(mascotaID)
     }
 
     //ArticleJOINTag
     @WorkerThread
-    suspend fun insert(at:ArticleJOINTag){
+    suspend fun insert(at: ArticleJOINTag) {
         ArticleJOINTagDAO.insert(at)
     }
-    fun getArticleOfTags(articleID:Int):LiveData<List<Article>>{
+
+    fun getArticleOfTags(articleID: Int): LiveData<List<Article>> {
         return ArticleJOINTagDAO.getArticleOfTags(articleID)
     }
 
-    fun getTagsOfArticle(tagID:Int): LiveData<List<Tag>>{
+    fun getTagsOfArticle(tagID: Int): LiveData<List<Tag>> {
         return ArticleJOINTagDAO.getTagsOfArticle(tagID)
     }
 
     //UserJOINArticle
     @WorkerThread
-    suspend fun insert(ua:UserJOINArticle){
+    suspend fun insert(ua: UserJOINArticle) {
         UserJOINArticleDAO.insert(ua)
     }
 
-    fun getUserOfArticles(userID:Int): LiveData<List<User>>{
+    fun getUserOfArticles(userID: Int): LiveData<List<User>> {
         return UserJOINArticleDAO.getUserOfArticles(userID)
     }
 
-    fun getArticlesOfUsers(articleID:Int): LiveData<List<Article>>{
+    fun getArticlesOfUsers(articleID: Int): LiveData<List<Article>> {
         return UserJOINArticleDAO.getArticlesOfUsers(articleID)
     }
 
