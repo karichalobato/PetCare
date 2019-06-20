658d9703a15e0f9822f5d7322b2d180e64f21847
Tania Orellana <00123717@uca.edu.sv>
2019-06-20 11:06:27 -0600

    Agregar converter para tratamiento de fechas.

--- app/src/main/java/com/cuidadoanimal/petcare/Database/PetCareRoomDatabase.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/Database/PetCareRoomDatabase.kt
@@ -4,40 +4,49 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.cuidadoanimal.petcare.Database.Entities.*
 import com.cuidadoanimal.petcare.Database.EntitiesDAO.*
+import com.example.gameon.data.database.converters.Converters
 
-@Database(entities = [User::class,
-    Pet::class,
-    Article::class,
-    Tag::class,
-    Vaccine::class,ArticleJOINTag::class,UserJOINArticle::class,VaccineJOINPet::class], version = 1, exportSchema = false)
-public abstract class PetCareRoomDatabase : RoomDatabase(){
+@Database(
+    entities = [User::class,
+        Pet::class,
+        Article::class,
+        Tag::class,
+        Vaccine::class, ArticleJOINTag::class, UserJOINArticle::class, VaccineJOINPet::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class PetCareRoomDatabase : RoomDatabase() {
 
-    abstract fun userDAO():UserDAO
-    abstract fun petDAO():PetDAO
-    abstract fun articleDAO():ArticleDAO
-    abstract fun tagDAO():TagDAO
-    abstract fun vaccineDAO():VaccineDAO
-    abstract fun vaccineJOINpetDAO():VaccineJOINPetDAO
-    abstract fun userJOINarticleDAO():UserJOINArticleDAO
-    abstract fun articleJOINtagDAO():ArticleJOINTagDAO
+    abstract fun userDAO(): UserDAO
+    abstract fun petDAO(): PetDAO
+    abstract fun articleDAO(): ArticleDAO
+    abstract fun tagDAO(): TagDAO
+    abstract fun vaccineDAO(): VaccineDAO
+    abstract fun vaccineJOINpetDAO(): VaccineJOINPetDAO
+    abstract fun userJOINarticleDAO(): UserJOINArticleDAO
+    abstract fun articleJOINtagDAO(): ArticleJOINTagDAO
 
-    companion object{
+    companion object {
         @Volatile
         private var INSTANCE: PetCareRoomDatabase? = null
 
         fun getInstance(
             context: Context
-        ):PetCareRoomDatabase{
+        ): PetCareRoomDatabase {
             val temInstance = INSTANCE
-            if(temInstance != null){
+            if (temInstance != null) {
                 return temInstance
             }
-            synchronized(this){
+            synchronized(this) {
                 val instance = Room
-                    .databaseBuilder(context, PetCareRoomDatabase::class.java,
-                        "PetCare_DataBase")
+                    .databaseBuilder(
+                        context, PetCareRoomDatabase::class.java,
+                        "PetCare_DataBase"
+                    )
                     .build()
                 INSTANCE = instance
                 return instance
