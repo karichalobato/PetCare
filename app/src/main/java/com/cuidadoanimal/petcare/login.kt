766aa1a11767d9d7973128593037ac888ba9d7b1
karichalobato <00002517@uca.edu.sv>
2019-06-22 22:08:04 -0600

    Modo Tutti ON

--- app/src/main/java/com/cuidadoanimal/petcare/login.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/login.kt
@@ -1,20 +1,16 @@
 package com.cuidadoanimal.petcare
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.DrawableRes
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
 import java.util.*
@@ -109,7 +105,7 @@ class login : Fragment() {
         startActivityForResult(
             AuthUI.getInstance().createSignInIntentBuilder()
                 .setAvailableProviders(providers)
-                .setLogo(R.drawable.cat)
+                .setLogo(R.drawable.ic_cat_logo_login)
                 .setTheme(R.style.MyTheme)
                 .build(), MY_REQUEST_CODE
         )
