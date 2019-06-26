f861f6b6684b78e8a87e83136e4bf00fadae5944
Tania Orellana <00123717@uca.edu.sv>
2019-06-26 02:30:56 -0600

    Agregar constraint a campo de e-mail de usuario en base de datos.

--- app/src/main/java/com/cuidadoanimal/petcare/data/database/entities/User.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/data/database/entities/User.kt
@@ -2,14 +2,17 @@ package com.cuidadoanimal.petcare.data.database.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "User")
+@Entity(tableName = "User",
+        indices = [Index(value = ["Email"],
+                unique = true)])
 data class User(
-    @ColumnInfo(name = "Name")
-    var name: String? = "N/A",
-    @ColumnInfo(name = "Email")
-    var email: String? = "N/A"
+        @ColumnInfo(name = "Name")
+        var name: String? = "N/A",
+        @ColumnInfo(name = "Email")
+        var email: String? = "N/A"
 ) {
     @PrimaryKey(autoGenerate = true)
     var idUser: Long = 0
