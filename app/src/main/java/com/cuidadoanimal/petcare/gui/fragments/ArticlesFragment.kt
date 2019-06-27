97e8ab01c574d6007180f05a09889a9c250c9d3c
karichalobato <00002517@uca.edu.sv>
2019-06-26 19:43:12 -0600

    Articles identificables con iconos

--- app/src/main/java/com/cuidadoanimal/petcare/gui/fragments/ArticlesFragment.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/gui/fragments/ArticlesFragment.kt
@@ -7,8 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import kotlinx.android.synthetic.main.fragment_articles.*
+import android.widget.ImageView
+import android.R
+import android.widget.Button
+import android.widget.GridLayout
 
-import com.cuidadoanimal.petcare.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,14 +41,19 @@ class ArticlesFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_articles, container, false)
+        return inflater.inflate(com.cuidadoanimal.petcare.R.layout.fragment_articles, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
