c67712736f4c36db44eea35d814657d7240950f3
Tania Orellana <00123717@uca.edu.sv>
2019-07-01 15:27:13 -0600

    Agregar ViewModel.

--- app/src/main/java/com/cuidadoanimal/petcare/data/repositories/PetCareRepository.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/data/repositories/PetCareRepository.kt
@@ -18,47 +18,49 @@ class PetCareRepository {
     var user = FirebaseAuth.getInstance().currentUser
 
     /** Guardar usuario*/
-    private fun insertUser(user: User) = getUser().set(user)
+    fun insertUser(user: User) = getUser().set(user)
 
     /** Obtener usuario actual */
-    private fun getUser() = db
-            .collection(USERS_COLLECTION_NAME)
-            .document(user!!.email.toString())
+    fun getUser() = db
+        .collection(USERS_COLLECTION_NAME)
+        .document(user!!.email.toString())
 
     /** Guardar mascota*/
-    private fun insertPet(pet: Pet) = getPet(pet.pet_name!!)
-            .set(pet)
+    fun insertPet(pet: Pet) = getPet(pet.pet_name!!)
+        .set(pet)
 
     /** Obtener mascotas*/
-    private fun getAllPets() = getUser()
-            .collection(PETS_COLLECTION_NAME)
+    fun getAllPets() = getUser()
+        .collection(PETS_COLLECTION_NAME)
 
     /** Obtener mascota específica */
-    private fun getPet(petName: String) = getAllPets()
-            .document(petName)
+    fun getPet(petName: String) = getAllPets()
+        .document(petName)
 
     /** Guardar vacuna*/
-    private fun insertVaccine(petName: String, vaccine: Vaccine) = getVaccine(petName, vaccine.vaccine_name!!)
-            .set(vaccine)
+    fun insertVaccine(petName: String, vaccine: Vaccine) = getVaccine(petName, vaccine.vaccine_name!!)
+        .set(vaccine)
 
     /** Obtener vacunas*/
-    private fun getAllVaccines(petName: String) = getPet(petName)
-            .collection(VACCINES_COLLECTION_NAME)
+    fun getAllVaccines(petName: String) = getPet(petName)
+        .collection(VACCINES_COLLECTION_NAME)
 
     /** Obtener vacuna específica */
-    private fun getVaccine(petName: String, vaccineName: String) = getAllVaccines(petName)
-            .document(vaccineName)
+    fun getVaccine(petName: String, vaccineName: String) = getAllVaccines(petName)
+        .document(vaccineName)
 
     /** Guardar aplicación */
-    private fun insertApplication(petName: String, vaccineName: String, application: Application) = getApplication(petName, vaccineName, application.application_date!!)
+    fun insertApplication(petName: String, vaccineName: String, application: Application) =
+        getApplication(petName, vaccineName, application.application_date!!)
             .set(application)
 
     /** Obtener aplicaciones */
-    private fun getAllApplications(petName: String, vaccineName: String) = getVaccine(petName, vaccineName)
-            .collection(APPLICATIONS_COLLECTION_NAME)
+    fun getAllApplications(petName: String, vaccineName: String) = getVaccine(petName, vaccineName)
+        .collection(APPLICATIONS_COLLECTION_NAME)
 
     /** Obtener aplicación específica */
-    private fun getApplication(petName: String, vaccineName: String, applicationDate: String) = getAllApplications(petName, vaccineName)
+    fun getApplication(petName: String, vaccineName: String, applicationDate: String) =
+        getAllApplications(petName, vaccineName)
             .document(applicationDate)
 
 }
--- app/src/main/java/com/cuidadoanimal/petcare/data/viewmodels/PetCareViewModel.kt
+++ app/src/main/java/com/cuidadoanimal/petcare/data/viewmodels/PetCareViewModel.kt
@@ -1,6 +1,49 @@
 package com.cuidadoanimal.petcare.data.viewmodels
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import com.cuidadoanimal.petcare.data.database.entities.Pet
+import com.cuidadoanimal.petcare.data.database.entities.User
+import com.cuidadoanimal.petcare.data.database.entities.Application
+import com.cuidadoanimal.petcare.data.database.entities.Vaccine
+import com.cuidadoanimal.petcare.data.repositories.PetCareRepository
 
-class PetCareViewModel(app: Application) : AndroidViewModel(app)
+class PetCareViewModel() : ViewModel() {
+
+    val TAG = "FIRESTORE_VIEW_MODEL"
+    var repository = PetCareRepository()
+
+    /** Guardar usuario*/
+    fun insertUser(user: User) = repository.insertUser(user)
+
+    /** Obtener usuario actual */
+    fun getUser() = repository.getUser()
+
+    /** Guardar mascota*/
+    fun insertPet(pet: Pet) = repository.insertPet(pet)
+
+    /** Obtener mascotas*/
+    fun getAllPets() = repository.getAllPets()
+
+    /** Obtener mascota específica */
+    fun getPet(petName: String) = repository.getPet(petName)
+
+    /** Guardar vacuna*/
+    fun insertVaccine(petName: String, vaccine: Vaccine) = repository.insertVaccine(petName, vaccine)
+
+    /** Obtener vacunas*/
+    fun getAllVaccines(petName: String) = repository.getAllVaccines(petName)
+
+    /** Obtener vacuna específica */
+    fun getVaccine(petName: String, vaccineName: String) = repository.getVaccine(petName, vaccineName)
+
+    /** Guardar aplicación */
+    fun insertApplication(petName: String, vaccineName: String, application: Application) =
+        repository.insertApplication(petName, vaccineName, application)
+
+    /** Obtener aplicaciones */
+    fun getAllApplications(petName: String, vaccineName: String) = repository.getAllApplications(petName, vaccineName)
+
+    /** Obtener aplicación específica */
+    fun getApplication(petName: String, vaccineName: String, applicationDate: String) =
+        repository.getApplication(petName, vaccineName, applicationDate)
+}
